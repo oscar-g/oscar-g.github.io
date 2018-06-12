8b8b28a67e2eb787e7cb91b99bfad993865c235d
Oscar Gonzalez <oscarg610@gmail.com>
2018-06-12 16:25:45 -0400

    Remove unused assets, change gulp build directory

--- gulpfile.js
+++ gulpfile.js
@@ -7,31 +7,35 @@ var del = require('del'),
     stylus = require('gulp-stylus'),
     pug = require('gulp-pug');
 
-gulp.task('assets', function(){
+gulp.task('assets', ['clean'], function(){
     return gulp.src('./src/assets/**/*')
-    .pipe(gulp.dest('./htdocs'));
+    .pipe(gulp.dest('./build'));
 });
 
-gulp.task('styles', function(){
+gulp.task('styles', ['clean'], function(){
     return gulp.src('./src/styl/screen.styl')
     .pipe(stylus())
-    .pipe(gulp.dest('./htdocs/css/'));
+    .pipe(gulp.dest('./build/css/'));
 });
 
-gulp.task('pages', function(){
+gulp.task('pages', ['clean'], function(){
     return gulp.src('./src/pages/**/*.pug')
     .pipe(pug({
         locals: {
             ga: process.env.GOOGLE_ANALYTICS_KEY,
         }
     }))
-    .pipe(gulp.dest('./htdocs/'));
+    .pipe(gulp.dest('./build/'));
 });
 
 gulp.task('watch', ['build'], function(){
     return gulp.watch('./src/**/*', ['build']);
 });
 
-gulp.task('build', ['assets', 'styles', 'pages']);
+gulp.task('clean', function() {
+    return del('./build/');
+});
+
+gulp.task('build', ['clean', 'assets', 'styles', 'pages']);
 
 gulp.task('default', ['build', 'watch']);
